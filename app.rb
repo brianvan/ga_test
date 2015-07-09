2fd60b9aaf4760ba473d6cc2deebb63569b03485
Brian Van <brianvan@gmail.com>
2015-07-09 19:19:28 -0400

    adding readme

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+#Note: the "require" below was recommended
 require 'json'
 
 get '/' do
@@ -10,9 +11,11 @@ get '/favorites' do
   File.read('data.json')
 end
 
+#originally a GET; changes data on the server so it should be a POST
 post '/favorites' do
   file = JSON.parse(File.read('data.json'))
   unless params[:name] && params[:oid]
+#note: unsuccessfully tried to include a different return status here, but... didn't work.
     return 'Invalid Request'
     end
   movie = { name: params[:name], oid: params[:oid] }
